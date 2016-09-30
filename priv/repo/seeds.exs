e666abdbe80035065c90e8347b86574ef61b26a8
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-09-30 17:39:32 +0300

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias MarketApi.Repo
+alias MarketApi.Market
+
+Enum.each(1..5, fn(_) ->
+  Repo.insert!(
+    %Market{
+      name: Faker.Company.name,
+      phone: Faker.Phone.EnUs.phone
+    }
+  )
+end)
+
+Repo.transaction fn ->
+  Repo.all(Market) |> Enum.each(fn(market) ->
+    Enum.each(1..20, fn(_) ->
+      product = %{
+        name: Faker.Commerce.product_name,
+        barcode: Faker.Code.isbn,
+        image: Faker.Avatar.image_url,
+        price: Faker.Commerce.price
+      }
+      new_product = Ecto.build_assoc(market, :products, Map.put(product, :market_id, market.id))
+      Repo.insert!(new_product)
+    end)
+  end)
+end
